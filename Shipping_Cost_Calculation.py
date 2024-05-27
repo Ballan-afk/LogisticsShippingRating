de62bdb413b07407d7c847f22bf80a910a94e409
Ballan-afk <150700660+Ballan-afk@users.noreply.github.com>
2024-05-27 16:19:31 +0000

    Update Shipping_Cost_Calculation.py

--- Shipping_Cost_Calculation.py
+++ Shipping_Cost_Calculation.py
@@ -1,1 +1,15 @@
+#Shipping Cost Calculator
+
+#input package  weight and shipping rate
+
+
+weight = float(input("Enter the package weight in kilograms: "))
+Shipping_rate =float(input("Enter the shipping rate in kilograms: "))
+
+#Calculate the shipping cost
+shipping_cost = weight * shipping_rate
+
+#Display the results
+print(f"Shipping Cost:{shipping_cost} USD")
+
 
